073a75ca82abf3f09ad118ed729ffafa1cb382f2
JayZenith <jayzenith1124@gmail.com>
2022-12-16 14:58:20 -0800

    need to make clear and adjust the grid and input

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 
 function divGrid(k){
     for(let i = 0; i < k; i++){
-        const div = document.createElement('div');
-        div.style.width = "80px";
-        div.style.height = "80px";
+        let div = document.createElement('div');
+        div.style.width = "50px";
+        div.style.height = "50px";
         div.style.background = "white";
-        div.style.border = "1px solid black";
+        div.style.border = "0px solid black";
         div.addEventListener("mouseover", (event) => {
             div.style.background = "black";
         });
@@ -14,8 +14,18 @@ function divGrid(k){
     }
 }
 
+function okd(k){
+    let nodes = document.getElementById("main").childNodes;
+    for(let i = 0; i < nodes.length; i++){
+        if(nodes[i].nodeName.toLocaleLowerCase() == `div`){
+            nodes[i].style.background = "white";
+        }
 
+    }
+}
 
-
-let i = 100;
+let userInput = prompt(`Enter amount of divs`);
+let i = userInput;
 divGrid(i);
+const reset = document.getElementById("btn");
+reset.addEventListener("click", okd);
